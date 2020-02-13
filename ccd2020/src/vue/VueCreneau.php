ffb76307c35085663daf1b4fdeba77441b1d08f9
Théo Gérard <theogerard66@gmail.com>
2020-02-13 19:57:06 +0100

    Changement du lien

--- ccd2020/src/vue/VueCreneau.php
+++ ccd2020/src/vue/VueCreneau.php
@@ -106,7 +106,7 @@ END;
         {
             $tab = $tab.'<div class="colonne"><h3 class="jour">'.$key.'</h3>';
             foreach ($jour as $c)
-                $tab = $tab.'<div class="creneau"><a href="'.$this->path.'/creneauDetail'"><p>'.$c->hDebut." - ".$c->hFin.'</p></a></div>';
+                $tab = $tab.'<div class="creneau"><a href="'.$this->path.'/creneauDetail'"><p>'.$c->hDebut.' - '.$c->hFin.'</p></a></div>';
             $tab = $tab.'</div>';
         }
         return $tab."</div>";
